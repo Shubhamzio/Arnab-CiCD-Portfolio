b3d14f85402e5a5e921b047d0b4519f09f6ac882
Shubham <shubhamchakraborty702@gmail.com>
2025-05-27 13:51:02 +0530

    lets see the change

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";  // Global styles
 // import ScrollToTop from './src/components/ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter  baseName="/Arnab-CiCD-Portfolio/">
+    <BrowserRouter basename="/Arnab-CiCD-Portfolio/">
     {/* <ScrollToTop/> */}
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
