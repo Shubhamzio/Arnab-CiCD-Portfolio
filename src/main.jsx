43709630c729d59b02d3fcc1bbddc437b4c4959f
Shubham <shubhamchakraborty702@gmail.com>
2025-06-21 14:00:43 +0530

    commit for basename changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";  // Global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Arnab-CiCD-Portfolio/">
+    <BrowserRouter>
     {/* <ScrollToTop/> */}
       <App />
     </BrowserRouter>
